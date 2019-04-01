8043637bae632fa2052584be87e41ae43988bd71
kotavy <tavius@nyu.edu>
2019-04-01 05:32:24 -0400

    Fix unit test

--- UnityProject/Assets/Editor/UnitTests.cs
+++ UnityProject/Assets/Editor/UnitTests.cs
@@ -14,14 +14,11 @@ namespace Tests
         {
             // Assert that a dartboard and dart exist in the scene
             Assert.IsTrue(GameObject.Find("DartBoard") != null);
-            Assert.IsTrue(GameObject.Find("Dart") != null);
+            Assert.IsTrue(GameObject.Find("DartPrefab") != null);
 
             // Assert that "Score" exists and it is initially 0
             Text scoreText = GameObject.Find("Score").GetComponent<Text>();
             Assert.IsTrue(scoreText.text == "Score: 0");
-
-
-
         }
 
         // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
