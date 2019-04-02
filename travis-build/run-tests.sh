b9b31754a6c53c134073919ca2301f73d2dd80a7
kotavy <tavius@nyu.edu>
2019-04-02 04:25:05 -0400

    Different travis test file

--- travis-build/run-tests.sh
+++ travis-build/run-tests.sh
@@ -16,13 +16,25 @@ echo "Running editor unit tests for ${PROJECT_PATH}"
   -silent-crashes \
   -logFile $(pwd)/unity.log \
   -projectPath "$(pwd)/${PROJECT_PATH}" \
-  -runEditorTests \
-  -editorTestsResultFile $(pwd)/travis-build/test.xml \
+  -testResults $(pwd)/test-results.xml \
+  -logFile \
   -quit
 
-rc0=$?
-echo test.xml
 echo "Unit test logs"
-cat $(pwd)/travis-build/test.xml
-# exit if tests failed
-if [ $rc0 -ne 0 ]; then { echo "Failed unit tests"; exit $rc0; } fi
+UNITY_EXIT_CODE=$?
+
+if [ $UNITY_EXIT_CODE -eq 0 ]; then
+  echo "Run succeeded, no failures occurred";
+elif [ $UNITY_EXIT_CODE -eq 2 ]; then
+  echo "Run succeeded, some tests failed";
+elif [ $UNITY_EXIT_CODE -eq 3 ]; then
+  echo "Run failure (other failure)";
+else
+  echo "Unexpected exit code $UNITY_EXIT_CODE";
+fi
+
+echo $(pwd)/test-results.xml
+cat $(pwd)/travis-build/test-results.xml
+cat $(pwd)/$TEST_PLATFORM-results.xml | grep test-run | grep Passed
+
+exit $UNITY_EXIT_CODE
